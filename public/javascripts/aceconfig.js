59ea54f49b6e6083ec403bae2038949b66a69e9f
tharinduketipe <katramesh@gmail.com>
2015-06-15 00:53:35 +0530

    layout changed

--- public/javascripts/aceconfig.js
+++ public/javascripts/aceconfig.js
@@ -1,21 +1,17 @@
+
 var editor = ace.edit("editor");
-    editor.setTheme("ace/theme/xcode");
+    editor.setTheme("ace/theme/solarized_dark");
     editor.getSession().setMode("ace/mode/yaml");
+    
+document.getElementById('editor').style.fontSize='15px';
 
 
 
 
 
-editor.getSession().on('change', function(e) {
-
-
-    nativeObject = YAML.parse(editor.getValue());
-    //window.alert(nativeObject);
-    console.log(nativeObject);
 
 
 
-});
 var code  = editor.getValue();
 nativeObject = YAML.parse(editor.getValue());
 console.log(nativeObject);
@@ -28,32 +24,12 @@ editor.getSession().on('change', function(e) {
         nativeObject = YAML.parse(editor.getValue());
         //window.alert(nativeObject);
         var out  = nativeObject.toString();
-        window.alert(editor.getValue());
+        //window.alert(editor.getValue());
         console.log(out);
 
     //}
 
 });
 
-var code  = editor.getValue();
-//console.log(code);
-//console.log(code);
 
-
-
-
-
-
-
-//nativeObject = YAML.parse(code);
-//console.log(nativeObject);
-
-//nativeObject = YAML.parse(code);
-
-
-
-// parse YAML string
-//nativeObject = YAML.parse(code);
-
-//console.log(nativeObject);
 
