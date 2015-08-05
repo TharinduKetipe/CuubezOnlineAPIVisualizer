f2ac2b92b49c42c0905f3c02d86839cdd44c88e3
tharinduketipe <katramesh@gmail.com>
2015-08-05 18:53:49 +0530

    integrated

--- public/javascripts/aceconfig.js
+++ public/javascripts/aceconfig.js
@@ -1,8 +1,10 @@
 
+
+
 var editor = ace.edit("editor");
     editor.setTheme("ace/theme/solarized_dark");
     editor.getSession().setMode("ace/mode/yaml");
-    
+
 document.getElementById('editor').style.fontSize='15px';
 
 
@@ -12,24 +14,53 @@ document.getElementById('editor').style.fontSize='15px';
 
 
 
-var code  = editor.getValue();
-nativeObject = YAML.parse(editor.getValue());
-console.log(nativeObject);
+//var code  = editor.getValue();
+//nativeObject = YAML.parse(editor.getValue());
+//console.log(nativeObject);
+
 
 
 
 editor.getSession().on('change', function(e) {
 
 
-        nativeObject = YAML.parse(editor.getValue());
-        //window.alert(nativeObject);
-        var out  = nativeObject.toString();
-        //window.alert(editor.getValue());
-        console.log(out);
 
-    //}
+
+
+
+
+
+
+try{
+    nativeObject = YAML.parse(editor.getValue());
+    //var out  = nativeObject.toString();
+    var out = JSON.stringify(nativeObject);
+    console.log(out);
+
+
+    $('#jsoncode').val(out);
+    $('#jsoncode').trigger('input');
+
+
+
+}catch(e2) {
+    //window.alert("Syntax Error");
+    swal("Syntax Error", "Please check your yaml syntaxes!", "error");
+
+
+
+}
+
+
+
+
 
 });
+
+
+
+
+
 
 
 
