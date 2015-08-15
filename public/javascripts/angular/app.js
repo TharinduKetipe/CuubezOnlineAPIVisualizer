f289caaacf73a25f0858a40a82ed7dfe2d8a1849
Matthias Eckhart <Matthias.Eckhart@edu.fh-joanneum.at>
2015-08-15 13:06:02 +0200

    [BUGFIX]: Fixed AngularJS implementation.

--- public/javascripts/angular/app.js
+++ public/javascripts/angular/app.js
@@ -1,12 +1,18 @@
 var myApp = angular.module('myApp', []);
 
-angular.module('myApp').directive('jsonInput', function() {
+angular.module('myApp').directive('jsonInput', function () {
     return {
         restrict: 'A',
         require: 'ngModel',
-        link: function(scope, element, attr, ngModel) {
-            function into(input) {return JSON.parse(input);}
-            function out(data) {return JSON.stringify(data);}
+        link: function (scope, element, attr, ngModel) {
+            function into(input) {
+                return JSON.parse(input);
+            }
+
+            function out(data) {
+                return JSON.stringify(data);
+            }
+
             ngModel.$parsers.push(into);
             ngModel.$formatters.push(out);
         }
