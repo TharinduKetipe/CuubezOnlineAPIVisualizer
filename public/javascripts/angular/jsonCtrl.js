fbbcbfd4d37f3a807405f7ca7a525e912022585c
Matthias Eckhart <Matthias.Eckhart@edu.fh-joanneum.at>
2015-08-16 10:53:09 +0200

    [FEATURE]: Implemented real-time update for editor.

--- public/javascripts/angular/jsonCtrl.js
+++ public/javascripts/angular/jsonCtrl.js
@@ -1,4 +1,17 @@
 function jsonCtrl($scope) {
-    $scope.myData = JSON.parse(localStorage.getItem("yaml"));
-    console.log($scope.myData);
+    $scope.myData = getJSON();
+    editor.getSession().on('change', function (e) {
+        try {
+            $scope.myData = getJSON();
+            $scope.$apply();
+        } catch (e2) {
+            //window.alert("Syntax Error");
+            swal("Syntax Error", "Please check your yaml syntaxes!", "error");
+        }
+    });
+
+    function getJSON() {
+        return JSON.parse(JSON.stringify(YAML.parse(editor.getValue())));
+    }
+
 }
