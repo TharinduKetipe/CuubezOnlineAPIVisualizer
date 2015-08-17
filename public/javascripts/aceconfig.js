f910fc38dddd34eabfe5601f562360760a094890
tharinduketipe <katramesh@gmail.com>
2015-08-18 01:15:38 +0530

    xml file download fixed

--- public/javascripts/aceconfig.js
+++ public/javascripts/aceconfig.js
@@ -3,3 +3,26 @@ editor.setTheme("ace/theme/solarized_dark");
 editor.getSession().setMode("ace/mode/yaml");
 
 document.getElementById('editor').style.fontSize = '15px';
+
+
+
+function convertJSon2XML() {
+
+    var x2js = new X2JS();
+    var input = JSON.parse(JSON.stringify(YAML.parse(editor.getValue())));
+//console.log(input);
+    var output =x2js.json2xml_str(input);
+//console.log(output);
+
+    var hiddenElement = document.createElement('a');
+
+    hiddenElement.href = 'data:attachment/xml,' + encodeURI(output);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'cuubez.xml';
+    hiddenElement.click();
+
+}
+
+$("#xml").click(convertJSon2XML);
+
+
